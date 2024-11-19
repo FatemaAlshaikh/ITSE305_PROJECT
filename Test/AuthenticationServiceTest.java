1ba4d4108de9656c00e820c79843d9e0600e2076
Fatema-Mohd <f6em.mohd012@gmail.com>
2024-11-19 20:23:01 +0300

    LoginUseCase

---
+++ Test/AuthenticationServiceTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthenticationServiceTest {
+
+    private UserRepository userRepository;
+    private AuthenticationService authService;
+
+    @Before
+    public void setUp() {
+        userRepository = new UserRepository();
+        authService = new AuthenticationService(userRepository);
+        userRepository.addUser(new User("test_user", "password123"));
+        userRepository.addUser(new User("admin", "adminPass"));
+    }
+
+    @Test
+    public void testSuccessfulAuthentication() {
+        assertTrue(authService.authenticate("test_user", "password123"));
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        assertFalse(authService.authenticate("test_user", "wrongPassword"));
+    }
+
+    @Test
+    public void testNonExistentUser() {
+        assertFalse(authService.authenticate("unknown_user", "anyPassword"));
+    }
+
+    @Test
+    public void testEmptyInputs() {
+        assertFalse(authService.authenticate("", ""));
+    }
+
+    @Test
+    public void testNullInputs() {
+        assertFalse(authService.authenticate(null, null));
+    }
+}
---
+++ Test/UserTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void testUserCreation() {
+        User user = new User("test_user", "test_password");
+        assertEquals("test_user", user.getUsername());
+        assertEquals("test_password", user.getPassword());
+    }
+
+    @Test
+    public void testSettersAndGetters() {
+        User user = new User("initial_user", "initial_password");
+        user.setUsername("updated_user");
+        user.setPassword("updated_password");
+        assertEquals("updated_user", user.getUsername());
+        assertEquals("updated_password", user.getPassword());
+    }
+}
