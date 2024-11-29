4a35dd2cc1f4456cfe57b29edf29dbcc5618f08f
Fatema-Mohd <f6em.mohd012@gmail.com>
2024-11-29 13:34:48 +0300

    LoginUseCase

--- Login UseCase/Login.java
+++ Login UseCase/Login.java
@@ -17,17 +17,14 @@ class User {
     public String getUsername() {
         return username;
     }
-
     // Setter for username
     public void setUsername(String username) {
         this.username = username;
     }
-
     // Getter for password
     public String getPassword() {
         return password;
     }
-
     // Setter for password
     public void setPassword(String password) {
         this.password = password;
