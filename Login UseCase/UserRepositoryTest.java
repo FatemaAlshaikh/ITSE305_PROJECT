d1f3c8696e20b72b97809c20e3883c1dfa03074a
Fatema-Mohd <f6em.mohd012@gmail.com>
2024-11-30 16:56:20 +0300

    LoginUseCase(UserRepositoryTest)

--- Login UseCase/UserRepositoryTest.java
+++ Login UseCase/UserRepositoryTest.java
@@ -14,6 +14,7 @@ public class UserRepositoryTest {
     @Test
     public void testAddAndFindUser() {
         User user = new User("test_user", "password123");
+        assertTrue(userRepository.addUser(user)); // first add should be succeed
         userRepository.addUser(user);
         User retrievedUser = userRepository.findByUsername("test_user");
         assertNotNull(retrievedUser);
@@ -25,5 +26,14 @@ public class UserRepositoryTest {
     public void testFindNonExistentUser() {
         User retrievedUser = userRepository.findByUsername("nonexistent_user");
         assertNull(retrievedUser);
+    }
+
+    @Test
+    public void testDuplicateUsers(){
+        User user1 = new User("duplicate_user", password123);
+        User user2 = new User("duplicate_user", password12345);
+
+        assertTrue(userRepository.addUser(user1)); // first add should succeed
+        assertFalse(userRepository.addUser(user2)); // second add with the same username should fail
     }
 }
