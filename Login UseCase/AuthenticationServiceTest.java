a75056b504bf326a1545783c085eb2f821f62a68
Fatema-Mohd <f6em.mohd012@gmail.com>
2024-11-30 10:27:04 +0300

    LoginUseCase

--- Login UseCase/AuthenticationServiceTest.java
+++ Login UseCase/AuthenticationServiceTest.java
@@ -1,8 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class AuthenticationServiceTest {
 
+
+public class AuthenticationServiceTest {
     private UserRepository userRepository;
     private AuthenticationService authService;
 
