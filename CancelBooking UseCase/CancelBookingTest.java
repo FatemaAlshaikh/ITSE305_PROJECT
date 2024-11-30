5e2ff24d755224eac53c3defb0d5df2f78d75650
Fatema Alshaikh <fatimashaikh5727@gmail.com>
2024-11-30 08:17:16 +0300

    cancel Booking test

---
+++ CancelBooking UseCase/CancelBookingTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDate;
+import static org.junit.Assert.*;
+
+
+
+public class CancelBookingTest {
+
+
+
+        @Test
+        public void testDisplayReservations() {
+            ReservationRepository repository = new ReservationRepository();
+            ReservationService service = new ReservationService(repository);
+            service.addReservation("Fatema Alshaikh", "Deluxe", "1", 150.0, "020900581", "00973-36784589", LocalDate.now());
+
+
+        
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(outputStream);
+            System.setOut(printStream);
+
+            
+
+            CancelBooking.displayReservations(service);
+            String output = outputStream.toString();
+            assertTrue(output.contains("Current Reservations"));
+            assertTrue(output.contains("Fatema Alshaikh"));
+                    }
+            
+    }
