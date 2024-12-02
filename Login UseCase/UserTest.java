b0babf2fb32df92c530315afa9862c695eb3512a
Fatema-Mohd <f6em.mohd012@gmail.com>
2024-12-02 12:24:49 +0300

    LoginUseCase(UserTest)

--- Login UseCase/UserTest.java
+++ Login UseCase/UserTest.java
@@ -14,7 +14,7 @@ public class UserTest {
         // ensure an incorrect password doesn’t match
         assertFalse(user.verifyPassword("password001"));
     }
-
+    
     @Test
     public void testPasswordUpdateAndVerification(){
         User user = new User("test_user", "Password123");
@@ -24,4 +24,12 @@ public class UserTest {
         assertTrue(user.verifyPassword("userPassword"));
         assertFalse(user.verifyPassword("Password123"));
     }
+
+    @Test
+    public void testEqualsAndHashCode() {
+    User user1 = new User("test_user", "password4564");
+    User user2 = new User("test_user", "Password09878");
+    assertEquals(user1, user2); // Same username, should be equal
+    assertEquals(user1.hashCode(), user2.hashCode());
+}
 }
